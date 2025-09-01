c55658ac4135b4db0b189862402c2dd696493c01
deep60 <deep6060p@gmail.com>
2025-09-02 00:07:55 +0530

    solve the issue in contract files

---
+++ blockchain/test/BountyManager.test.ts
@@ -0,0 +1,14 @@
+// import { expect } from "chai";
+// import { ethers } from "hardhat";
+// import { Contract, Signer } from "ethers";
+
+// describe("BountyManager", function () {
+//   let bountyManager: contrats;
+//   let threatToken: ThreatToken;
+//   let reputationSystem: ReputationSystem;
+//   let owner: Signer;
+//   let analyst1: Signer;
+//   let analyst2: Signer;
+//   let submitter: Signer;
+//   let addrs: Signer[];
+// }
